8ea6db7ada78942a19d4a3af9f24d046ebd975fa
Alexander <strrife@gmail.com>
2016-01-18 12:39:10 +0300

    Fixed translation and file location

--- app/index.js
+++ app/index.js
@@ -1,24 +1,24 @@
 var yeoman = require('yeoman-generator');
 
 var Generator = module.exports = yeoman.generators.Base.extend({
-    constructor: function(){
-        yeoman.generators.Base.apply(this, arguments);
-    },
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+  },
 
-    init: function () {
-        console.log('There is no logic at aurelia-base generator itself');
-        console.log('please try:');
-        console.log(' ');
-        console.log('aurelia-base:component NAME');
-        console.log('aurelia-base:example NAME');
-        console.log('aurelia-base:view NAME');
-        console.log(' ');
-        console.log('NOTE:');
-        console.log('Name can be a path');
-        console.log(' ');
-        console.log('Example:');
-        console.log('aurelia-base:component folder/name');
-    }
+  init: function () {
+    console.log('There is no logic in aurelia-base generator itself');
+    console.log('please try:');
+    console.log(' ');
+    console.log('aurelia-base:component NAME');
+    console.log('aurelia-base:example NAME');
+    console.log('aurelia-base:view NAME');
+    console.log(' ');
+    console.log('NOTE:');
+    console.log('Name can be a path');
+    console.log(' ');
+    console.log('Example:');
+    console.log('aurelia-base:component folder/name');
+  }
 });
 
 Generator.name = "Generator Aurelia Base";
