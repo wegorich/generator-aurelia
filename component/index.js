26bffa27e5dac0e62bc3049315deb4e9135a499d
Alexander <strrife@gmail.com>
2016-01-18 12:38:54 +0300

    Added test generation

--- component/index.js
+++ component/index.js
@@ -4,23 +4,33 @@ var basePath = 'src/_components/';
 var helpers = require('../helpers');
 var path = helpers.path;
 
-module.exports = generators.NamedBase.extend({
-    generateAureliaVM: function () {
-        this.fs.copyTpl(
-            this.templatePath('vm.js'),
-            this.destinationPath(path(this.name, '.js', basePath)),
-            {Name: helpers.toCamelCase(this.name)}
-        );
-    },
 
-    generateAureliaView: function () {
-        this.fs.copyTpl(
-            this.templatePath('view.html'),
-            this.destinationPath(path(this.name, '.html', basePath)),
-            {
-                Name: helpers.toCamelCase(this.name),
-                Value: '${value}'
-            }
-        );
-    }
+
+module.exports = generators.NamedBase.extend({
+  generateAureliaVM: function () {
+    this.fs.copyTpl(
+      this.templatePath('vm.js'),
+      this.destinationPath(path(this.name, '.js', basePath)),
+      {Name: helpers.toCamelCase(this.name)}
+    );
+  },
+
+  generateAureliaTest: function () {
+    this.fs.copyTpl(
+      this.templatePath('test.js'),
+      this.destinationPath(path(this.name, '.spec.js', basePath)),
+      {Name: helpers.toCamelCase(this.name)}
+    );
+  },
+
+  generateAureliaView: function () {
+    this.fs.copyTpl(
+      this.templatePath('view.html'),
+      this.destinationPath(path(this.name, '.html', basePath)),
+      {
+        Name: helpers.toCamelCase(this.name),
+        Value: '${value}'
+      }
+    );
+  }
 });
---
+++ component/templates/test.js
@@ -0,0 +1,7 @@
+var assert = require("assert");
+
+describe('<%= Name %>', function() {
+  it('Should pass a dummy test', function () {
+    assert(true);
+  });
+});
